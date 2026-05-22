77f29b3ccb220328274a1ab2332efdced1e134c8
Alphatech-1 <alfaropanayfocristhian@gmail.com>
2026-05-21 19:15:36 -0500

    qaletaDashBoard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,46 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwind(),
+
+      VitePWA({
+        registerType: "autoUpdate",
+
+        manifest: {
+          name: "Qaleta",
+          short_name: "Qaleta",
+
+          description:
+            "Sistema de control de ventas y gastos",
+
+          theme_color: "#111827",
+          background_color: "#111827",
+
+          display: "standalone",
+          start_url: "/dashboard.astro",
+
+          icons: [
+            {
+              src: "/qaleta.jpg",
+              sizes: "192x192",
+              type: "image/jpg",
+            },
+            {
+              src: "/qaleta.jpg",
+              sizes: "512x512",
+              type: "image/jpg",
+            },
+          ],
+        },
+
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
+  },
+});
